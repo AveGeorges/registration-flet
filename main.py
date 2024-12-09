8aca40bccf925263a3f18a840ecfeabc27301b72
George <smoly1995@gmail.com>
2024-12-09 18:34:50 +0300

    add personal account

--- main.py
+++ main.py
@@ -54,6 +54,14 @@ def main(page: fl.Page):
             user_password_field.value = ''
             page.snack_bar = fl.SnackBar(fl.Text('Вы успешно вошли!'))
             page.snack_bar.open = True
+            
+            if len(page.navigation_bar.destinations) == 2:
+              page.navigation_bar.destinations.append(fl.NavigationBarDestination(
+                icon=fl.icons.BOOK,
+                label='Кабинет',
+                selected_icon=fl.icons.BOOKMARK
+              ))
+            
             page.update()
         else:
             page.snack_bar = fl.SnackBar(fl.Text('Введены неверные данные'))
@@ -74,8 +82,30 @@ def main(page: fl.Page):
       elif page.navigation_bar.selected_index == 1:
         page.add(panel_auth)
         
-    
-    
+      elif page.navigation_bar.selected_index == 2:
+        user_list.controls.clear()
+        
+        db = sqlite3.connect('myusers.db')
+        cursor = db.cursor()
+        
+        cursor.execute("SELECT * FROM users ")
+        result = cursor.fetchall()
+        
+        if result != None:
+          for user in result:
+            user_list.controls.append(fl.Row([
+              fl.Text(f'User {user[1]}'),
+              fl.Icon(fl.icons.VERIFIED_USER_ROUNDED)
+            ]
+                                             ))
+            
+        cursor.close()    
+        db.commit()
+        db.close()
+        
+        page.add(panel_cabinet)
+        
+        
     user_login_field = fl.TextField(label='Имя пользователя', width=200, on_change=validate)
     user_password_field = fl.TextField(label='Пароль', width=200, on_change=validate)
     confirm_reg_button = fl.OutlinedButton(text='Подтвердить', width=200, disabled=True, on_click=register)
@@ -84,6 +114,8 @@ def main(page: fl.Page):
     panel_start = fl.Row(
           [fl.Text('Добро пожаловать в YourApp!')], alignment=fl.MainAxisAlignment.CENTER
         )
+       
+    user_list = fl.ListView(spacing=10, padding=20)
     
     panel_register = fl.Row(
             [
@@ -93,6 +125,18 @@ def main(page: fl.Page):
                         user_login_field,
                         user_password_field,
                         confirm_reg_button
+                    ]
+                )
+            ],
+            alignment=fl.MainAxisAlignment.CENTER
+        )
+    
+    panel_cabinet = fl.Row(
+            [
+                fl.Column(
+                    [
+                        fl.Text('Личный кабинет'),
+                        user_list
                     ]
                 )
             ],
@@ -125,10 +169,3 @@ def main(page: fl.Page):
 fl.app(target=main)
 
 # https://www.youtube.com/watch?v=JJCjAUmNXBs
-
-# page.navigation_bar.destinations.pop(0) убрать кнопку авторизации при переходе в ЛК
-#             page.navigation_bar.destinations.pop(0)
-#             page.clean()
-#             page.add(panel_cabine)
-
-# https://translated.turbopages.org/proxy_u/en-ru.ru.7f1852d0-67531e7a-978fdb24-74722d776562/https/www.geeksforgeeks.org/implement-a-stack-using-singly-linked-list/
